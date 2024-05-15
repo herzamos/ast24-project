5ddd18f1314efb7e0f9590139faab375333e6736
Alessandro Legnani <alegnani@ethz.ch>
2024-05-15 13:42:43 +0200

    dfg of fold works

--- vecspot/src/dfg.rs
+++ vecspot/src/dfg.rs
@@ -1,13 +1,110 @@
+use std::collections::hash_map::Entry;
 use std::fs::File;
 use std::io::Write;
 use std::{collections::HashMap, fmt::Debug};
 
 use graphviz_rust::{cmd::Format, exec_dot};
+use petgraph::graph::NodeIndex;
 use petgraph::{dot::Dot, graph::DiGraph, Graph};
 
 use crate::trace::{TraceMemOpType, TraceOperation, TracePoint};
 
-pub type Dfg = Graph<Operand, String>;
+// pub type Dfg = Graph<Operand, String>;
+
+pub struct Dfg {
+    graph: Graph<Operand, String>,
+    reg_map: HashMap<String, NodeIndex>,
+    mem_map: HashMap<u64, NodeIndex>,
+}
+
+impl Dfg {
+    fn new() -> Self {
+        Self {
+            graph: DiGraph::new(),
+            mem_map: HashMap::new(),
+            reg_map: HashMap::new(),
+        }
+    }
+
+    fn access_reg(&mut self, reg_name: String, read: bool) -> NodeIndex {
+        match self.reg_map.entry(reg_name.clone()) {
+            Entry::Occupied(o) => {
+                if read {
+                    *o.get()
+                } else {
+                    let new_idx = self.graph.add_node(Operand::Register(reg_name.clone()));
+                    self.reg_map.insert(reg_name, new_idx);
+                    new_idx
+                }
+            }
+            Entry::Vacant(v) => {
+                let name = if read {
+                    format!("{}**", &reg_name)
+                } else {
+                    reg_name
+                };
+                let idx = self.graph.add_node(Operand::Register(name));
+                v.insert(idx);
+                idx
+            }
+        }
+    }
+
+    fn access_mem(&mut self, mem_addr: u64, read: bool) -> NodeIndex {
+        match self.mem_map.entry(mem_addr) {
+            Entry::Occupied(o) => {
+                if read {
+                    *o.get()
+                } else {
+                    let new_idx = self.graph.add_node(Operand::Memory(mem_addr));
+                    self.mem_map.insert(mem_addr, new_idx);
+                    new_idx
+                }
+            }
+            Entry::Vacant(v) => {
+                let idx = self.graph.add_node(Operand::Memory(mem_addr));
+                v.insert(idx);
+                idx
+            }
+        }
+    }
+
+    fn add_trace_point(&mut self, trace_point: TracePoint) {
+        match trace_point.op {
+            TraceOperation::Pass => (),
+            TraceOperation::BinOp(op) => {
+                let reg1 = self.reg_map.get(&op.reg1);
+                let reg2 = self.reg_map.get(&op.reg2);
+
+                // this skips registers that have not been written by to by
+                // an access to memory
+                if reg1.is_none() || reg2.is_none() {
+                    return;
+                }
+                let reg1_idx = self.access_reg(op.reg1, true);
+                let reg2_idx = self.access_reg(op.reg2.clone(), true);
+
+                let reg3_idx = self.access_reg(op.reg2, false);
+
+                self.graph.add_edge(reg1_idx, reg3_idx, op.op.clone());
+                self.graph.add_edge(reg2_idx, reg3_idx, op.op.clone());
+            }
+            TraceOperation::MemOp(op) => {
+                let is_read = match op.typ {
+                    TraceMemOpType::Read => true,
+                    TraceMemOpType::Write => false,
+                };
+                let mem_idx = self.access_mem(op.addr, is_read);
+                let reg_idx = self.access_reg(op.reg.clone(), !is_read);
+                if is_read {
+                    self.graph.add_edge(mem_idx, reg_idx, "Read".into());
+                } else {
+                    self.graph.add_edge(reg_idx, mem_idx, "Write".into());
+                }
+            }
+        }
+    }
+}
 
 pub trait DfgOperations {
     fn from_trace(trace: Vec<TracePoint>) -> Self;
@@ -16,71 +113,29 @@ pub trait DfgOperations {
 
 impl DfgOperations for Dfg {
     fn from_trace(trace: Vec<TracePoint>) -> Self {
-        let mut graph: Graph<Operand, String> = DiGraph::new();
-
-        let mut mem_map = HashMap::new();
-        let mut reg_map = HashMap::new();
+        let mut s = Self::new();
 
         for trace_point in trace {
-            // TODO: this is dogshit pls fix
-            match trace_point.op {
-                TraceOperation::Pass => (),
-                TraceOperation::BinOp(op) => {
-                    let reg1 = reg_map.get(&op.reg1);
-                    let reg2 = reg_map.get(&op.reg2);
-
-                    // this skips registers that have not been written by to by
-                    // an access to memory
-                    if reg1.is_none() || reg2.is_none() {
-                        continue;
-                    }
-                    let new_reg2 = graph.add_node(Operand::Register(op.reg2.clone()));
-                    graph.add_edge(*reg1.unwrap(), new_reg2, op.op.clone());
-                    graph.add_edge(*reg2.unwrap(), new_reg2, op.op.clone());
-                    reg_map.insert(op.reg2, new_reg2);
-                }
-                TraceOperation::MemOp(op) => {
-                    let mem = match mem_map.get(&op.addr) {
-                        None => {
-                            let index = graph.add_node(Operand::Memory(op.addr));
-                            mem_map.insert(op.addr, index);
-                            index
-                        }
-                        Some(x) => *x,
-                    };
-
-                    match op.typ {
-                        TraceMemOpType::Read => {
-                            let reg_idx = graph.add_node(Operand::Register(op.reg.clone()));
-                            reg_map.insert(op.reg, reg_idx);
-                            graph.add_edge(mem, reg_idx, "Read".into());
-                        }
-                        TraceMemOpType::Write => {
-                            let reg = *reg_map.get(&op.reg).unwrap();
-                            graph.add_edge(reg, mem, "Write".into());
-                            mem_map.insert(op.addr, graph.add_node(Operand::Memory(op.addr)));
-                        }
-                    };
-                }
-            }
+            s.add_trace_point(trace_point);
         }
 
         // remove memory locations that are not used
-        graph.filter_map(
+        s.graph.filter_map(
             |i, node| {
-                if graph.neighbors_undirected(i).next().is_none() {
+                if s.graph.neighbors_undirected(i).next().is_none() {
                     None
                 } else {
                     Some((*node).clone())
                 }
             },
             |_, edge| Some(edge.clone()),
-        )
+        );
+        s
     }
 
     fn to_png(&self, name: &str) {
         let png = exec_dot(
-            format!("{:?}", Dot::with_config(self, &[])),
+            format!("{:?}", Dot::with_config(&self.graph, &[])),
             vec![Format::Png.into()],
         )
         .unwrap();
